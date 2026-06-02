92808db3a4ee9fc87ed59a6b318fda3dd737e0ef
LazyCode2 <21darkwarrior@gmail.com>
2026-06-02 17:28:11 +0600

    feat: add program counter module

--- src/computer.js
+++ src/computer.js
@@ -1,11 +1,30 @@
+import {ProgramCounter} from "./module/PC"
+
 class Computer {
-	constructor () {
+	constructor() {
 		this.bus = 0x00;
+
+		this.pc = new ProgramCounter();
+
 		this.currentTstate = 1;
 	}
 
 	reset() {
-        this.bus = 0x00;
-        this.currentTState = 1;
-    }
+      this.bus = 0x00;
+      his.currentTState = 1;
+  }
+
+  step() {
+  	this.bus = 0x00;
+  	this.pc.tick(true);
+  }
+
+  // For testing
+  getSnapshot() {
+	  return {
+	      bus: this.bus,
+	      tState: this.currentTState,
+	      pc: this.pc.value
+	  };
+  }
 }
---
+++ src/module/PC.js
@@ -0,0 +1,19 @@
+class ProgramCounter {
+	constructor () {
+		this.value = 0x0
+	}
+
+	reset () {
+		this.value = 0x0
+	}
+
+	/**
+     * Updates the register state on a clock cycle trigger
+     * @param {boolean} cp - Count increment active
+    */
+    tick (cp) {
+    	if (cp == true) {
+    		this.value = (this.value + 1) & 0x0F;
+    	}	
+    }
+}
