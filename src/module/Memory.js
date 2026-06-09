6b8774bea4d792c892f4c53a3fe1e9750c0108a4
LazyCode2 <21darkwarrior@gmail.com>
2026-06-09 12:16:27 +0600

    fix: loading value from bus instead of PC

--- src/module/Memory.js
+++ src/module/Memory.js
@@ -1,15 +1,11 @@
-import { ProgramCounter } from "./PC.js"
-
-var pc = new ProgramCounter();
-
 class MAR {
 	constructor () {
 		this.value = 0x0;
 	}
 
-	load (lm) {
+	load (lm, busValue) {
 		if (lm == true) {
-			this.value = pc.value;
+			this.value = busValue & 0x0F;
 		}
 	}
 }
