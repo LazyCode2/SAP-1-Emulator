3f981da66773cc43000e0bee40f61812aec8dcbd
LazyCode2 <21darkwarrior@gmail.com>
2026-06-10 11:40:41 +0600

    feat: add microstep instruction execution

--- src/computer.js
+++ src/computer.js
@@ -1,6 +1,6 @@
 import { ProgramCounter } from "./module/PC.js"
 import { MAR , RandomAccessMemory } from "./module/Memory.js"
-import { InstructionRegister } from "./module/Register.js"
+import { InstructionRegister , RegisterAccumulator } from "./module/Register.js"
 
 export class Computer {
 	constructor() {
@@ -10,8 +10,10 @@ export class Computer {
 		this.mar = new MAR();
 		this.memory = new RandomAccessMemory();
 		this.ir = new InstructionRegister();
+		this.a = new RegisterAccumulator();
 
-		this.memory.write(true,0,1)
+		this.memory.write(true,0,0x16);
+		this.memory.write(true,6,0x06);
 		this.currentTstate = 1;
 	}
 
@@ -23,6 +25,7 @@ export class Computer {
   step() {
   	this.bus = 0x00;
   	
+  	// TODO: Add better instruction excution 
   	switch(this.currentTstate) {
   		case 1:
   			this.bus = this.pc.value;
@@ -35,12 +38,34 @@ export class Computer {
 			
 			case 3:
 				this.bus = this.memory.drive(true,this.mar.value);
-				this.ir.load(true,this.bus)
+				this.ir.load(true,this.bus);
 				break
-
+			case 4:
+				// Microstep
+				switch(this.ir.getOpcode()){
+					// LOAD
+					case 0x1:
+						this.bus = this.ir.getOperand();
+			            this.mar.load(true, this.bus);
+			            break;
+				}
+				break;
+			case 5:
+				// Microstep
+				switch(this.ir.getOpcode()){
+					// LOAD
+					case 0x1:
+						this.bus = this.memory.drive(true, this.mar.value);
+			            this.a.load(true, this.bus);
+			            break;
+				}
+				break;
+			case 6:
+				break
   	}
+
   	this.currentTstate++;
-  	if (this.currentTstate > 3) {
+  	if (this.currentTstate > 6) {
         this.currentTstate = 1;
     }
   }
@@ -52,6 +77,7 @@ export class Computer {
 	      tState: this.currentTstate,
 	      pc: this.pc.value,
 	      ir: this.ir.value,
+	      a: this.a.value,
 	      mar: this.mar.value,
 	      memory: this.memory
 	  };
